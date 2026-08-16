746f1355753cbc16f89c071dc80c56aa6ef4c5f5
MrMustafa <mr.mohdmustafa007@gmail.com>
2026-08-15 22:58:18 -0500

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
@@ -0,0 +1,45 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {} # for every charecter a is mapped to trie node ie a : TrieNode
+        self.word = False
+
+
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode() #initially there is no charecters in the trienode
+
+
+    def addWord(self, word: str) -> None:
+        curr = self.root
+
+        for c in word: # so this basically does is shift every charecters in the word to the trie node
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.word = True
+
+
+    def search(self, word: str) -> bool:
+
+        def dfs(j, root):
+            curr = root
+
+            for i in range(j, len(word)):
+                c = word[i]
+
+                if c == ".":
+                    for child in curr.children.values(): 
+                        if dfs(i + 1, child):
+                            return True
+                    return False
+
+                else:
+                    if c not in curr.children:
+                        return False
+                    curr = curr.children[c]
+            return curr.word
+        
+        return dfs(0, self.root)
+        
